c4741f79f52aef4bbb5339c29ea3826afa37b6cb
yadsashel <ysalhi600@gmail.com>
2024-02-13 13:19:44 -0800

    test_review.py

--- tests/test_review.py
+++ tests/test_review.py
@@ -7,7 +7,7 @@ Unittest classes:
     TestReview_to_dict
 """
 import os
-import models
+from models.engine.file_storage import FileStorage
 import unittest
 from datetime import datetime
 from time import sleep
